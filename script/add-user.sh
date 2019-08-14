292a1a0dd0ece32975980da0943d1ee8d45f29ad
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-08-14 12:26:35 +0200

    basic auth scripts adaptation for mounted .htaccess

--- script/add-user.sh
+++ script/add-user.sh
@@ -4,5 +4,5 @@ then
   echo "Usage: $0 <USER_NAME> <USER_PASSWORD>" >&2
   exit 1
 fi
-echo -n "$1:" >> /etc/nginx/.htpasswd
-openssl passwd $2 >> /etc/nginx/.htpasswd
+echo -n "$1:" >> /conf/.htpasswd
+openssl passwd $2 >> /conf/.htpasswd
--- script/remove-user.sh
+++ script/remove-user.sh
@@ -4,4 +4,4 @@ then
   echo "Usage: $0 <USER_NAME>" >&2
   exit 1
 fi
-sed -i "/$1:.*/d" /etc/nginx/.htpasswd
+sed -i "/$1:.*/d" /conf/.htpasswd
